1627eb7a28b0b6d6e2fccb3605816f1dc79b5ae0
Carlo Tacchella <tacchella.carlo@gmail.com>
2023-12-26 12:25:03 +0100

    filter map findindex

--- lezione02 - rendering condizionale/my-app/src/App.js
+++ lezione02 - rendering condizionale/my-app/src/App.js
@@ -4,11 +4,27 @@ import './App.css';
 
 const Animals = ["Dog", "Cat", "Frog"];
 console.log(Animals[0]);
+const Animals2 = [["Dog", "Cat", "Frog"],[]];
+console.log(Animals2[0][1]);
+const index = Animals.findIndex((item)=>item ==="Frog");
+console.log("indice:"+index);
+const Animals3 = Animals.map((item)=>item+"!");
+console.log(Animals3);
+const ages = [32, 33, 16, 40];
+
+let checkAdult2=(a)=>a>=18;
+function checkAdult(age) {
+  return age >= 18;
+}
+let result = ages.filter(checkAdult);
+console.log(result);
+result = ages.filter(checkAdult2);
+console.log(result);
 
 function App() {
   return (
     <div>
-  
+
     </div>
   );
 }
