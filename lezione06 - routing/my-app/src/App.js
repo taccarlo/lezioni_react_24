fecd84e30d73407f2ba1928195703525a5b6e03f
Carlo Tacchella <tacchella.carlo@gmail.com>
2023-12-26 11:23:43 +0100

    [UP] installare pacchetto e definire pagina home

--- lezione06 - routing/my-app/src/App.js
+++ lezione06 - routing/my-app/src/App.js
@@ -1,14 +1,14 @@
 
 import './App.css';
-
+import {createBrowserRouter, RouterProvider} from 'react-router-dom';
+import HomePage from './Page/Home';
+const router = createBrowserRouter ([
+  {path:'/', element: <HomePage/>},
+]);
 //differenti path caricano differenti pagine
-
+// npm install react-router-dom
 function App() {
-  return (
-    <div>
-      
-    </div>
-  );
+  return <RouterProvider router={router}/>;
 }
 
 export default App;
