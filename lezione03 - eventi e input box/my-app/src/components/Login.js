fb3414e1da77c323a24edc3e4d9b5d83d256b912
Carlo Tacchella <tacchella.carlo@gmail.com>
2023-12-27 10:16:03 +0100

    Lezione 03 step 1

---
+++ lezione03 - eventi e input box/my-app/src/components/Login.js
@@ -0,0 +1,27 @@
+export default function Login() {
+    function handleSubmit(){
+      console.log("Submitted!");
+    }
+    return (
+      <form>
+        <h2>Login</h2>
+  
+        <div className="control-row">
+          <div className="control no-margin">
+            <label htmlFor="email">Email</label>
+            <input id="email" type="email" name="email" />
+          </div>
+  
+          <div className="control no-margin">
+            <label htmlFor="password">Password</label>
+            <input id="password" type="password" name="password" />
+          </div>
+        </div>
+  
+        <p className="form-actions">
+          <button className="button" onClick={handleSubmit}>Login</button>
+        </p>
+      </form>
+    );
+  }
+  
