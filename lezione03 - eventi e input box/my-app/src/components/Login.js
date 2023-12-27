08bf7b2a39c84bc055639c262718f8929b293dce
Carlo Tacchella <tacchella.carlo@gmail.com>
2023-12-27 10:59:56 +0100

    usestate multiple object

--- lezione03 - eventi e input box/my-app/src/components/Login.js
+++ lezione03 - eventi e input box/my-app/src/components/Login.js
@@ -5,36 +5,36 @@
 import { useState } from "react";
 
 export default function Login() {
-  const [enteredEmail, setEnteredEmail] = useState();
-  const [enteredPassword, setEnteredPassword] = useState();
+  const [enteredValues, setEnteredValues] = useState({
+    email:'',
+    password:''
+  });
 
   function handleSubmit(event) {
     event.preventDefault();
     console.log("Submitted!");
-    alert("Submitted!");
   }
 
-  function handleEmailChange(event){
-    setEnteredEmail(event.target.value);
+  function handleInputChange(identifier, event){
+    setEnteredValues(prevValues => ({
+      ...prevValues,
+      [identifier]:event.target.value
+    }));
   }
 
-  function handlePasswordChange(event){
-    setEnteredPassword(event.target.value);
-  }
 
   return (
     <form onSubmit={handleSubmit}>
       <h2>Login</h2>
-
       <div className="control-row">
         <div className="control no-margin">
           <label htmlFor="email">Email</label>
-          <input id="email" type="email" name="email" onChange={handleEmailChange} value={enteredEmail}/>
+          <input id="email" type="email" name="email" onChange={(event)=>handleInputChange('email', event)} value={enteredValues.email}/>
         </div>
 
         <div className="control no-margin">
           <label htmlFor="password">Password</label>
-          <input id="password" type="password" name="password"onChange={handlePasswordChange} value={enteredPassword} />
+          <input id="password" type="password" name="password" onChange={(event)=>handleInputChange('password', event)} value={enteredValues.password} />
         </div>
       </div>
 
