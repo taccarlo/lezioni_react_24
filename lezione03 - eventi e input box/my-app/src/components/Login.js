64840f2611931922c28377ea77f261c7f0e2e27f
Carlo Tacchella <tacchella.carlo@gmail.com>
2023-12-27 10:40:54 +0100

    Update Login.js

--- lezione03 - eventi e input box/my-app/src/components/Login.js
+++ lezione03 - eventi e input box/my-app/src/components/Login.js
@@ -2,32 +2,45 @@
 /* questo è dato dal fatto che viene inviata al server una richiesta http al server locale che gestisce il sito*/
 /* con type = button non viene sottomessa al server sul bottone, oppure mettere onSubmit sul form */
 //con preventdefault preveniamo il comportamento di default del browser
+import { useState } from "react";
+
 export default function Login() {
-    function handleSubmit(event){
-      event.preventDefault();
-      console.log("Submitted!");
-      alert("Submitted!");
-    }
-    return (
-      <form  onSubmit={handleSubmit}>
-        <h2>Login</h2>
-  
-        <div className="control-row">
-          <div className="control no-margin">
-            <label htmlFor="email">Email</label>
-            <input id="email" type="email" name="email" />
-          </div>
-  
-          <div className="control no-margin">
-            <label htmlFor="password">Password</label>
-            <input id="password" type="password" name="password" />
-          </div>
-        </div>
-  
-        <p className="form-actions">
-          <button className="button">Login</button>
-        </p>
-      </form>
-    );
+  const [enteredEmail, setEnteredEmail] = useState();
+  const [enteredPassword, setEnteredPassword] = useState();
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    console.log("Submitted!");
+    alert("Submitted!");
   }
-  
+
+  function handleEmailChange(event){
+    setEnteredEmail(event.target.value);
+  }
+
+  function handlePasswordChange(event){
+    setEnteredPassword(event.target.value);
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <h2>Login</h2>
+
+      <div className="control-row">
+        <div className="control no-margin">
+          <label htmlFor="email">Email</label>
+          <input id="email" type="email" name="email" onChange={handleEmailChange} value={enteredEmail}/>
+        </div>
+
+        <div className="control no-margin">
+          <label htmlFor="password">Password</label>
+          <input id="password" type="password" name="password"onChange={handlePasswordChange} value={enteredPassword} />
+        </div>
+      </div>
+
+      <p className="form-actions">
+        <button className="button">Login</button>
+      </p>
+    </form>
+  );
+}
